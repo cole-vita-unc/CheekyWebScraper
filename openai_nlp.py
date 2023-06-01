2ba916842d4ce97c740d9f836acb9eda10269c1e
Cole <cole.vita@unc.edu>
2023-06-01 10:35:52 -0400

    Added documentation and config

--- openai_nlp.py
+++ openai_nlp.py
@@ -1,9 +1,20 @@
 #Set your OPENAI_API_KEY environment variable by adding the following line into your shell initialization 
 #script (e.g. .bashrc, zshrc, etc.) or running it in the command line before the fine-tuning command:
     #export OPENAI_API_KEY="<OPENAI_API_KEY>"
+import config
 import openai
 
 def updateWithNLP(extracted_info):
+    """
+    The function takes extracted information, generates NLP output, parses it, and returns updated
+    information.
+    
+    :param extracted_info: It is a variable that contains information extracted from a source, such as a
+    text document or a database. This information can be in the form of text, numbers, or other data
+    types. The function "updateWithNLP" takes this extracted information as input and uses natural
+    language processing (NLP
+    :return: the updated information after processing it with NLP.
+    """
     # Generate NLP output
     string_response = nlpOutput(extracted_info)
 
@@ -13,6 +24,19 @@ def updateWithNLP(extracted_info):
     return updated_info
 
 def nlpOutput(extracted_fields):
+    """
+    This function takes in extracted fields from a product description and uses OpenAI's
+    text-davinci-003 engine to generate a prompt asking for details about the product, returning the
+    generated response.
+    
+    :param extracted_fields: The parameter extracted_fields is a dictionary containing the extracted
+    fields from a product description. It is used to retrieve the product title, which is then used as
+    input for the NLP model to identify the product details
+    :return: The function `nlpOutput` takes in a dictionary `extracted_fields` as input and returns a
+    string that contains the product details extracted from the given product title using OpenAI's GPT-3
+    language model. If the product title is None, the function returns the string 'No product title
+    found'.
+    """
 
     product_title = extracted_fields['TITLE']
 
@@ -56,7 +80,7 @@ def nlpOutput(extracted_fields):
     Description: {product_title}
     """
 
-    openai.api_key = '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'
+    openai.api_key = config.OPENAI_KEY
 
     response = openai.Completion.create(
     engine="text-davinci-003",
@@ -67,6 +91,18 @@ def nlpOutput(extracted_fields):
     return response.choices[0].text.strip()
 
 def parseOutput(extracted_info, string_response):
+    """
+    The function parses a response string and updates a dictionary with extracted information.
+    
+    :param extracted_info: The `extracted_info` parameter is a dictionary containing information that
+    has already been extracted from a response string. The keys of the dictionary represent the type of
+    information (e.g. "Title", "Brand", "Type") and the values represent the extracted
+    information for each key. If a key
+    :param string_response: The string response is a string containing information that needs to be
+    parsed and matched with keys in a dictionary
+    :return: the updated `extracted_info` dictionary with new key-value pairs added based on the
+    `string_response` input. The function also prints the number of keys that were updated.
+    """
     keys_updated = 0
     # Parse response string
     for line in string_response.split('\n'):
@@ -75,7 +111,7 @@ def parseOutput(extracted_info, string_response):
             key = key.upper()  # Make key uppercase to match dictionary keys
             # Check for the key in the dictionary in a case-insensitive manner
             for info_key in extracted_info:
-                if info_key.upper() == key and extracted_info[info_key] is None and value != 'Not specified':
+                if info_key.upper() == key and extracted_info[info_key] is None and value.lower() != 'not specified':
                     extracted_info[info_key] = value
                     keys_updated += 1
                     break
